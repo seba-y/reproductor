55076d8bd200eb57af73ea9a833aab9ba8fe5546
seba-y <rapettiyoel@gmail.com>
2024-03-26 18:44:49 -0300

    canciones

--- js/canciones.js
+++ js/canciones.js
@@ -1,61 +1,61 @@
 let canciones = [
     {
         nombre: "Putting on the ritz",
-        direccion: "js/songs/2 - YSY A FT QUEVEDO - LUGARES QUE LLEGO (PROD. ONIRIA).mp3",
+        direccion: "js/songs/Putting On The Ritz - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "Take me out to the ballgame",
-        direccion: "js/songs/6 - YSY A FT JERE KLEIN - 24_7 6.5 (PROD. ONIRIA).mp3",
+        direccion: "js/songs/Take Me Out To The Ballgame - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "Thou swell",
-        direccion: "js/songs/08  - YSY A x BHAVI ft. MILO J - FLECHAZO EN EL CENTRO (PROD. ONIRIA).mp3",
+        direccion: "js/songs/Thou Swell - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "When irish eyes are smiling",
-        direccion: "js/songs/Roze x Peipper  INOCENTE  (Video Oficial).mp3",
+        direccion: "js/songs/When Irish Eyes Are Smiling - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "You scream i scream we all scream for ice cream",
-        direccion: "js/songs/YOUNG MIKO-BZRP Music Sessions 58.mp3",
+        direccion: "js/songs/You Scream I Scream We All Scream For Ice Cream - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "My blue heaven",
-        direccion: "js/songs/MILO J BZRP Music Sessions 57.mp3",
+        direccion: "js/songs/My Blue Heaven - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "I wonder whose kissing her now",
-        direccion: "js/songs/08  - YSY A x BHAVI ft. MILO J - FLECHAZO EN EL CENTRO (PROD. ONIRIA).mp3",
+        direccion: "js/songs/I Wonder Whose Kissing Her Now - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "Camptown races",
-        direccion: "js/songs/Roze x Peipper  INOCENTE  (Video Oficial).mp3",
+        direccion: "js/songs/Camptown Races - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "Chicago",
-        direccion: "js/songs/YOUNG MIKO-BZRP Music Sessions 58.mp3",
+        direccion: "js/songs/Chicago - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     },
     {
         nombre: "Give my regards to broadway",
-        direccion: "js/songs/MILO J BZRP Music Sessions 57.mp3",
+        direccion: "js/songs/Give My Regards To Broadway - Freedom Trail Studio.mp3",
         artista: "Freedom Trail Studio",
         imagen: "https://yt3.googleusercontent.com/AZbkcbAPAE5aeo_KNbWdL6hfBiYahj8S4iIGGpaG7C6XDiEusQAnni-NAfqvljglA_Vh4wtV7pQ=s176-c-k-c0x00ffffff-no-rj"
     }
